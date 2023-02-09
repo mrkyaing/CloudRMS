a3eae51872913bbc3d20ce4f4f1bf9d09174736d
mrkyaing <mr.kyaing7@gmail.com>
2023-02-10 02:45:15 +0630

    JQuery Ajax called to controller.

---
+++ CloudRMS/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using CloudRMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CloudRMS.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string TellMeDate()
+        {
+            return DateTime.Now.ToString();
+        }
+        public ViewResult MakeOrder()=>View();
+       
+        [HttpPost]
+        public JsonResult MakeOrder(OrderModel orderModel)
+        {
+            orderModel.OrderDate = DateTime.Now;
+            return Json(orderModel);
+        }
+    }
+}
---
+++ CloudRMS/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CloudRMS.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public DateTime  OrderDate { get; set; }
+    }
+}
